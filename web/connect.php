810addd6c7339b62c3d046ea37179c8925810a40
Louis Borreill <louis.borreill@tpc18.cpe.fr>
2020-10-14 15:19:17 +0200

    modification d el'emplacement du form de connection

--- web/connect.php
+++ web/connect.php
@@ -1,13 +1,8 @@
-<form method="post" action="connect.php">
-	<input type="text" name="ID">
-	<input type="password" name="PASS"> 
-	<input type="submit" value="Connexion" name="Connexion"> <!-- assign a name for the button -->
-</form>
 
 <?php
 
 if(isset($_POST)){
 	echo "ID ".$_POST['ID'];
-	echo "ID ".$_POST['PASS'];
+	echo "PASS ".$_POST['PASS'];
 }
 ?>
