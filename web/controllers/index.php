0739e27edb6b28310e29d76fb3ad9a2ec3012cc7
Quentin Gouy <quentin.gouy@tpc19.cpe.fr>
2020-10-14 17:26:35 +0200

    Correction affichage produit

--- web/controllers/index.php
+++ web/controllers/index.php
@@ -1,8 +1,7 @@
 <?php
     $bdd = new PDO("mysql:host=f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;port=3306;dbname=c2as0sld1uv3e09n", "vyduy5wuyry0lrw1",  "hbtajkck31kwohut");
     $res = $bdd->query('Select * From Products');
-    //$smarty->assign('products',$res);
-    $smarty->assign('toto',15);
+    $smarty->assign('products',$res);
     /*
         <table>
     <tr>
