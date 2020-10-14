55579d8c2066a331417427642fea00f1ddf6ff8b
Quentin Gouy <quentin.gouy@tpc19.cpe.fr>
2020-10-14 14:58:33 +0200

    Correction space

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ session_start();
 require('./libs/smarty/Smarty.class.php');
 
 try {
-    $bdd = new PDO("mysql:host=f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com 	;port=3306;dbname=c2as0sld1uv3e09n", "vyduy5wuyry0lrw1",  "hbtajkck31kwohut");
+    $bdd = new PDO("mysql:host=f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;port=3306;dbname=c2as0sld1uv3e09n", "vyduy5wuyry0lrw1",  "hbtajkck31kwohut");
     $bdd->query("SET NAMES UTF8");
 } catch (Exception $e) {	
     echo $e;
