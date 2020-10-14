9ddd518e1444bba97a74d46d54167996baa94db1
Quentin Gouy <quentin.gouy@tpc19.cpe.fr>
2020-10-14 15:20:05 +0200

    Get result of query

--- web/index.php
+++ web/index.php
@@ -1,4 +1,4 @@
-cho<?php
+<?php
 session_start();
 
 require('./libs/smarty/Smarty.class.php');
@@ -6,8 +6,12 @@ require('./libs/smarty/Smarty.class.php');
 try {
     $bdd = new PDO("mysql:host=f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;port=3306;dbname=c2as0sld1uv3e09n", "vyduy5wuyry0lrw1",  "hbtajkck31kwohut");
     $bdd->query("SET NAMES UTF8");
-	$res = $bdd->query('Select * From Products');
-	echo $res;
+    $res = $bdd->query('Select * From Products');
+    foreach($res as $row){
+      echo $row;
+    }
+    echo $res;
+    
 } catch (Exception $e) {	
     echo $e + "\n";
     echo "Problème de connexion à la base de donnée !";
