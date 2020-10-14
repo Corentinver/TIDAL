c99289c929fd1ecb8c32d9d06031051ec75beb4e
Louis Borreill <louis.borreill@tpc18.cpe.fr>
2020-10-14 17:11:41 +0200

    test search DB

--- web/connect.php
+++ web/connect.php
@@ -13,7 +13,7 @@ if(isset($_POST)){
 	    $bdd = new PDO("mysql:host=f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;port=3306;dbname=c2as0sld1uv3e09n", "vyduy5wuyry0lrw1",  "hbtajkck31kwohut");
 	    $bdd->query("SET NAMES UTF8");
 	    $res = $bdd->query("Select firstname,familyname From Customers WHERE username='george'");
-	    echo "<p>".$res."</p>";
+	    echo "Prénom + Nom".$res;
 	    //foreach($res as $row){
 	    //  echo "<p>".$row['firstname']."</p>";
 	    //}
