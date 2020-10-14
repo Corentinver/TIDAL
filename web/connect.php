a223e270ce078507a520b34b0ff3797ede004ab6
Louis Borreill <louis.borreill@tpc18.cpe.fr>
2020-10-14 16:26:55 +0200

    Erreur: parcours d'un tableau alors que la réponse n'est pas un tableau

--- web/connect.php
+++ web/connect.php
@@ -13,9 +13,10 @@ if(isset($_POST)){
 	    $bdd = new PDO("mysql:host=f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;port=3306;dbname=c2as0sld1uv3e09n", "vyduy5wuyry0lrw1",  "hbtajkck31kwohut");
 	    $bdd->query("SET NAMES UTF8");
 	    $res = $bdd->query('Select firstname,familyname From Customers WHERE firstname='.$_POST['ID']);
-	    foreach($res as $row){
-	      echo "<p>".$row['firstname']."</p>";
-	    }
+	    echo "<p>".$res['firstname']."</p>";
+	    //foreach($res as $row){
+	    //  echo "<p>".$row['firstname']."</p>";
+	    //}
 	
 	} catch (Exception $e) {
 	    echo $e + "\n";
