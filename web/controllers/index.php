55bc9d6c12034e65947b81f18cf418f5301ee594
Quentin Gouy <quentin.gouy@tpc19.cpe.fr>
2020-10-14 17:07:17 +0200

    Correction routing

--- web/controllers/index.php
+++ web/controllers/index.php
@@ -1,4 +1,20 @@
 <?php
     $res = $bdd->query('Select * From Products');
     $smarty->assign('products',$res);
+
+    /*
+        <table>
+    <tr>
+        <th>Name<th>
+        <th>Price<th>
+        <th>Description<th>
+    </tr>
+    {foreach from=products item=product}
+    <tr>
+        <td>{$product}<td>
+        <td>{$product}<td>
+        <td>{$product}<td>
+    </tr>
+    {/foreach}
+    */
 ?>
