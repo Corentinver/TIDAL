d158e0da98e82eaf8f53c245911cf5066d6397d2
Quentin Gouy <quentin.gouy@tpc19.cpe.fr>
2020-10-14 17:21:03 +0200

    Add connection to controller

--- web/controllers/index.php
+++ web/controllers/index.php
@@ -1,4 +1,5 @@
 <?php
+    $bdd = new PDO("mysql:host=f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;port=3306;dbname=c2as0sld1uv3e09n", "vyduy5wuyry0lrw1",  "hbtajkck31kwohut");
     $res = $bdd->query('Select * From Products');
     //$smarty->assign('products',$res);
     $smarty->assign('toto',15);
