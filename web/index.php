29bfbe05dd4c0e4eedd072b74b3a7a737d5b0c1d
Quentin Gouy <quentin.gouy@tpc19.cpe.fr>
2020-10-14 15:54:45 +0200

    Table presentation

--- web/index.php
+++ web/index.php
@@ -7,13 +7,19 @@ try {
     $bdd = new PDO("mysql:host=f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;port=3306;dbname=c2as0sld1uv3e09n", "vyduy5wuyry0lrw1",  "hbtajkck31kwohut");
     $bdd->query("SET NAMES UTF8");
     $res = $bdd->query('Select * From Products');
+    echo "<table border='1'>
+    <tr>
+    <th>Name<th>
+    <th>Name<th>
+    <th>Name<th>
+    <tr>";
     foreach($res as $row){
-      echo $row['name'];
-      echo $row['price'];
-      echo $row['description'];
+      echo "<td>".$row['name']."<td>";
+      echo "<td>".$row['price']."<td>";
+      echo "<td>".$row['description']."<td>";
       echo "</br>";
     }
-    echo $res + '\t';
+    echo "</table>";
     
 } catch (Exception $e) {	
     echo $e + "\n";
