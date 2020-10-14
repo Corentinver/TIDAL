14114fd7c4dd0989dc03e68c9fe7355f1aa8509d
Quentin Gouy <quentin.gouy@tpc19.cpe.fr>
2020-10-14 15:10:38 +0200

    get products

--- web/index.php
+++ web/index.php
@@ -7,11 +7,14 @@ try {
     $bdd = new PDO("mysql:host=f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;port=3306;dbname=c2as0sld1uv3e09n", "vyduy5wuyry0lrw1",  "hbtajkck31kwohut");
     $bdd->query("SET NAMES UTF8");
 } catch (Exception $e) {	
-    echo $e;
+    echo $e + "\n";
     echo "Problème de connexion à la base de donnée !";
     die();
 }
 
-echo("Ça marche !!!");
 
+
+echo("Ça marche !!!");
+$res = $bdd->query('Select * From Products');
+echo $res;
 include 'menu.php';
