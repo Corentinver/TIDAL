40be7968651f1c591eda5082e189fbe95ae8045a
Quentin Gouy <quentin.gouy@tpc19.cpe.fr>
2020-10-14 17:36:21 +0200

    Correction bug

--- web/controllers/index.php
+++ web/controllers/index.php
@@ -1,7 +1,6 @@
 <?php
     $bdd = new PDO("mysql:host=f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;port=3306;dbname=c2as0sld1uv3e09n", "vyduy5wuyry0lrw1",  "hbtajkck31kwohut");
     $res = $bdd->query('Select * From Products');
-    $res.exec();
     $smarty->assign('products',$res);
     /*
         <table>
