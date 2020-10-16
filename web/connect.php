868e141cd7b685d136e94eb74e06b7efaa3ba307
Louis Borreill <louis.borreill@tpc21.cpe.fr>
2020-10-16 09:05:23 +0200

    debug

--- web/connect.php
+++ web/connect.php
@@ -14,7 +14,7 @@ if(isset($_POST)){
 		$bdd = new PDO("mysql:host=f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;port=3306;dbname=c2as0sld1uv3e09n", "vyduy5wuyry0lrw1",  "hbtajkck31kwohut");
     		$bdd->query("SET NAMES UTF8");
 		
-		$res = $bdd->query('Select firstname,familyname From Customers WHERE username="'.$ID'"');
+		$res = $bdd->query('Select firstname,familyname From Customers WHERE username="'.$ID.'"');
 		print_r($res);
 		echo " end of research";
 		//foreach($res as $row){
@@ -22,9 +22,9 @@ if(isset($_POST)){
 	    	//}
 	
 	} catch (Exception $e) {
-	    echo $e + "\n";
-	    echo "Problème de connexion à la base de donnée !";
-	    die();
+		echo $e + "\n";
+		echo "Problème de connexion à la base de donnée !";
+		die();
 	}
 }
 ?>
