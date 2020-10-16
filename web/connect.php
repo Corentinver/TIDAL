3a0512f7464e39397fcde963a6d8b81b30d468f6
Louis Borreill <louis.borreill@tpc21.cpe.fr>
2020-10-16 09:13:04 +0200

    debug

--- web/connect.php
+++ web/connect.php
@@ -14,7 +14,7 @@ if(isset($_POST)){
 		$bdd = new PDO("mysql:host=f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;port=3306;dbname=c2as0sld1uv3e09n", "vyduy5wuyry0lrw1",  "hbtajkck31kwohut");
     		$bdd->query("SET NAMES UTF8");
 		
-		$bdd->query('Select firstname,familyname From Customers WHERE username="'.$ID.'"');
+		$bdd->prepare('Select firstname,familyname From Customers WHERE username="'.$_POST['ID'].'"');
 		$bdd->execute();
 		$res=$bdd;
 		print_r($res);
