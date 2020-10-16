12f678b49afc98d34d41ad7bf2972296a0c3b52c
Louis Borreill <louis.borreill@tpc21.cpe.fr>
2020-10-16 09:43:08 +0200

    hash password

--- web/connect.php
+++ web/connect.php
@@ -4,20 +4,20 @@
 if(isset($_POST)){
 	echo "ID :".$_POST['ID'];
 	echo "PASS :".$_POST['PASS'];
-
+	$hash = md5($_POST['PASS']);
 	session_start();
 	
 	require('./libs/smarty/Smarty.class.php');
 	
 	try {
-	    $bdd = new PDO("mysql:host=f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;port=3306;dbname=c2as0sld1uv3e09n", "vyduy5wuyry0lrw1",  "hbtajkck31kwohut");
-	    $bdd->query("SET NAMES UTF8");
-	    $res = $bdd->query("Select * From Customers Where username='george'");
-	    echo "Prénom + Nom".$res['firstname'];
-	    //foreach($res as $row){
-	    //  echo "<p>".$row['firstname']."</p>";
-	    //}
-	
+	    	$bdd = new PDO("mysql:host=f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;port=3306;dbname=c2as0sld1uv3e09n", "vyduy5wuyry0lrw1",  "hbtajkck31kwohut");
+	    	$bdd->query("SET NAMES UTF8");
+		$res = $bdd->prepare('Select firstname,familyname From Customers WHERE username="'.$_POST['ID'].'" AND WHERE password="'.$hash.'"');
+		$res->execute();
+		$res = $res->fetch(PDO::FETCH_OBJ);
+		echo $res->firstname." ".$res->familyname;
+		
+
 	} catch (Exception $e) {
 	    echo $e + "\n";
 	    echo "Problème de connexion à la base de donnée !";
