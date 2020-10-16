f6a21e7252e7f9d529409b1af3d2654f2908c90a
Quentin Gouy <quentin.gouy@tpc22.cpe.fr>
2020-10-16 10:44:11 +0200

    debug

--- web/config/database_conf.php
+++ web/config/database_conf.php
@@ -2,7 +2,7 @@
 
 
 // Define de l'adresse de la base de données
-define('DRIVER', 'mysql')
+define('DRIVER', 'mysql');
 
 define('HOST', 'f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com');
 
