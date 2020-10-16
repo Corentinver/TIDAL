92832bfd571ad7cc613cb7dd9d1758f29a991d36
Louis Borreill <louis.borreill@tpc21.cpe.fr>
2020-10-16 08:29:38 +0200

    Debug

--- web/connect.php
+++ web/connect.php
@@ -11,12 +11,12 @@ if(isset($_POST)){
 	require('./libs/smarty/Smarty.class.php');
 	
 	try {
-	    	echo "test co BDD";
 	    	$bdd = new PDO("mysql:host=f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;port=3306;dbname=c2as0sld1uv3e09n", "vyduy5wuyry0lrw1",  "hbtajkck31kwohut");
     	    	$bdd->query("SET NAMES UTF8");
 	    	$res = $bdd->query("Select firstname,familyname From Customers WHERE username='george'");
-	    echo "Prénom + Nom".$res;
-	    //foreach($res as $row){
+	    	echo "Prénom + Nom".$res;
+		echo "end of research";
+		//foreach($res as $row){
 	    //  echo "<p>".$row['firstname']."</p>";
 	    //}
 	
