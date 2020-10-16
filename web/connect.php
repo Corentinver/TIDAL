a1a5f1b7bb03a02c49e5092c876f70504e5ed64b
Louis Borreill <louis.borreill@tpc21.cpe.fr>
2020-10-16 08:25:38 +0200

    debug recherche SQL

--- web/connect.php
+++ web/connect.php
@@ -11,9 +11,10 @@ if(isset($_POST)){
 	require('./libs/smarty/Smarty.class.php');
 	
 	try {
-	    $bdd = new PDO("mysql:host=f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;port=3306;dbname=c2as0sld1uv3e09n", "vyduy5wuyry0lrw1",  "hbtajkck31kwohut");
-	    $bdd->query("SET NAMES UTF8");
-	    $res = $bdd->query("Select firstname,familyname From Customers WHERE username='george'");
+	    	echo "test co BDD";
+	    	$bdd = new PDO("mysql:host=f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;port=3306;dbname=c2as0sld1uv3e09n", "vyduy5wuyry0lrw1",  "hbtajkck31kwohut");
+    	    	$bdd->query("SET NAMES UTF8");
+	    	$res = $bdd->query("Select firstname,familyname From Customers WHERE username='george'");
 	    echo "Prénom + Nom".$res;
 	    //foreach($res as $row){
 	    //  echo "<p>".$row['firstname']."</p>";
