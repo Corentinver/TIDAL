f956783d1a9f8e15d3183abc60c191cf69af80bb
Gouyq <55273887+Gouyq@users.noreply.github.com>
2020-10-31 17:58:33 +0100

    Single product

--- web/models/product.php
+++ web/models/product.php
@@ -11,7 +11,7 @@ class Product {
         $db = Database::init();
         /*$query = $db->connect()->prepare("SELECT `*` FROM `Products` WHERE id=:id");
         $query->bindParam(":id",$id);*/
-        return $db->connect()->prepare("SELECT `*` FROM `Products` WHERE `id=$id`");
+        return $db->connect()->query("SELECT `*` FROM `Products` WHERE `id=$id`");
     }
 
     public function addBasket($params){
