7a2bfa5fad75f05c7adf0dd30f3bda840b5ac947
Gouyq <55273887+Gouyq@users.noreply.github.com>
2020-11-07 02:11:05 +0100

    Update cart.php

--- web/cart.php
+++ web/cart.php
@@ -4,7 +4,7 @@
  if(isset($_POST['action']) && !empty($_POST['action'])){
     switch($_POST['action']){
         case 'addToCart' :
-            addToCart($_POST['price'], $_POST['qty']);
+            addToCart($_POST['id'],$_POST['price'], $_POST['qty']);
             break;
     }
 }
@@ -14,38 +14,38 @@
 function createCart(){
     if (!isset($_SESSION['cart'])){
        $_SESSION['cart']=array();
-       $_SESSION['cart']['libelleProduit'] = array();
-       $_SESSION['cart']['qteProduit'] = array();
-       $_SESSION['cart']['prixProduit'] = array();
+       $_SESSION['cart']['id'] = array();
+       $_SESSION['cart']['qty'] = array();
+       $_SESSION['cart']['price'] = array();
        $_SESSION['cart']['verrou'] = false;
     }
     return true;
  }
  
- function addToCart($price,$qty){
-    /*
+ function addToCart($id,$price,$qty){
+    
     //Si le cart existe
-    if (creationcart() /*&& !isVerrouille())
+    if (creationcart() /*&& !isVerrouille()*/)
     {
        //Si le produit existe déjà on ajoute seulement la quantité
-       $positionProduit = array_search($libelleProduit,  $_SESSION['cart']['libelleProduit']);
+       $positionProduit = array_search($id,  $_SESSION['cart']['id']);
  
        if ($positionProduit !== false)
        {
-          $_SESSION['cart']['qteProduit'][$positionProduit] += $qteProduit ;
+          $_SESSION['cart']['qty'][$positionProduit] += $qty ;
        }
        else
        {
           //Sinon on ajoute le produit
-          array_push( $_SESSION['cart']['libelleProduit'],$libelleProduit);
-          array_push( $_SESSION['cart']['qteProduit'],$qteProduit);
-          array_push( $_SESSION['cart']['prixProduit'],$prixProduit);
+          array_push( $_SESSION['cart']['id'],$id);
+          array_push( $_SESSION['cart']['qty'],$qty);
+          array_push( $_SESSION['cart']['price'],$price);
        }
     }
     else
     echo "Un problème est survenu veuillez contacter l'administrateur du site.";
-    */
-    echo $price . ' ' . $qty;;
+
+    var_dump($_SESSION['cart']);
  }
 ?>
 
